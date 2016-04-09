2eba0a65687c24d86597b73511c81998e49dbfe8
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-04-09 10:52:17 +0200

    Updated to v2.0
    Created a new project to rewrite app in Swift

---
+++ Webmontag/Webmontag/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Webmontag
+//
+//  Created by Johannes Jakob on 09.04.2016.
+//  Copyright © 2016 Johannes Jakob. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
